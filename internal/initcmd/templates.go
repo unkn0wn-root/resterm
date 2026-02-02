b34cf7b192f086b116fff785f497e6253f170d11
unkn0wn-root <unkn0wn_root@tuta.io>
2026-02-02 17:38:15 +0100

    refactor: resterm init

--- internal/initcmd/templates.go
+++ internal/initcmd/templates.go
@@ -6,6 +6,20 @@ import (
 	"unicode/utf8"
 )
 
+type TemplateStore interface {
+	Find(name string) (template, bool)
+	List() []template
+	Names() []string
+	Width() int
+}
+
+type BuiltinTemplates struct{}
+
+func (BuiltinTemplates) Find(name string) (template, bool) { return findTemplate(name) }
+func (BuiltinTemplates) List() []template                  { return templateList() }
+func (BuiltinTemplates) Names() []string                   { return templateNames() }
+func (BuiltinTemplates) Width() int                        { return templateWidth() }
+
 var helpMD = buildHelpMD()
 
 type tplCache struct {
